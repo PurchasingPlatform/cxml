7739ad9a9dc1ed08c222d27812b27a4d3565d030
Mike Mulev <m.mulev@gmail.com>
2018-03-29 19:11:24 +0300

    add comments node to item

--- lib/cxml/item.rb
+++ lib/cxml/item.rb
@@ -2,7 +2,7 @@ module CXML
   class Item
     attr_accessor :qty, :supplier_id, :supplier_auxiliary_id,
                   :unit_price, :description, :unit_of_measure,
-                  :unspsc, :lang, :name
+                  :unspsc, :lang, :name, :comments
 
     def render(node)
       node.ItemIn(quantity: qty) do
@@ -19,6 +19,8 @@ module CXML
           node.UnitOfMeasure(unit_of_measure)
           node.Classification(unspsc, domain: 'UNSPSC') unless unspsc.nil?
         end
+
+        node.Comments(comments) if comments
       end
     end
   end
